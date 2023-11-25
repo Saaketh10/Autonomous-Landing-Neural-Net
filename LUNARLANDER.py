aac2349c3a55a691c708976521c63b6652496f72
Saaketh10 <saakethsuvarna07@gmail.com>
2023-11-25 17:16:33 -0600

    save

--- LUNARLANDER.py
+++ LUNARLANDER.py
@@ -1,15 +1,18 @@
-import gym
-from stable_baselines import ACER #ACER is a machine learning type algorithim
-from stable_baselines.common.vec_env import DummyVecEnv #Dummy Vectorized Environment, some algorithms require a vectorized environment, so this works
-from stable_baselines.common.evaluation import evaluate_policy #Function used to evauluate performance of agent. Calculates Mean and Standard Deviation of all rewards obtained
-               
+import gymnasium as gym
+from stable_baselines3 import DQN 
+#from stable_baselines.common.vec_env import DummyVecEnv #Dummy Vectorized Environment, some algorithms require a vectorized environment, so this works
+from stable_baselines3.common.evaluation import evaluate_policy #Function used to evauluate performance of agent. Calculates Mean and Standard Deviation of all rewards obtained
+
+
 #BUILDING MODEL
 environment_name = 'LunarLander-v2'
-env = gym.make(environment_name) #Creates Environment
-env = DummyVecEnv([lambda: env]) #Wraps the Environment in the Dummy Vectorized Environment
-model = ACER('MlpPolicy', env, verbose = 1) # Creates an ACER model with a Multi-Layer Perceptron (MLP) policy 
+env = gym.make(environment_name, render_mode="rgb_array") #Creates Environment
+#env = DummyVecEnv([lambda: env]) #Wraps the Environment in the Dummy Vectorized Environment
+obs = env.reset()
+assert isinstance(env.action_space, gym.spaces.Discrete)
+model = DQN('MlpPolicy', env,verbose = 1) # Creates an ACER model with a Multi-Layer Perceptron (MLP) policy 
 #When Verbose=1, it prints training info of each episode
-model.learn(total_timesteps=100000) # Trains the model for a specified number of timesteps
+model.learn(total_timesteps=10000) # Trains the model for a specified number of timesteps
 
 
 
@@ -20,7 +23,8 @@ evaluate_policy(model, env, n_eval_episodes=10, render=True) #Evaluates 10 episo
 
 #SAVING MODEL 
 env.close()
-model.save("ACER_model")
+model.save("DQN_model")
 del model
-model = ACER.load("ACER_model", env=env) #Reloading Model to restart
+model = DQN.load("DQN_model", env=env) #Reloading Model to restart
     #We Want High Variance and High Possible Mean Reward
+
--- test.py
+++ test.py
@@ -1,24 +1,24 @@
-import gym
+import gym 
 from stable_baselines import ACER
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.common.evaluation import evaluate_policy
 
 environment_name = 'LunarLander-v2'
-env = gym.make(environment_name)
+env = gym.make(environment_name, render_mode="rgb_array") #Creates Environment
+#env = DummyVecEnv([lambda: env]) #Wraps the Environment in the Dummy Vectorized Environment
+obs = env.reset()
+model = ACER('MlpPolicy', env,verbose = 1) # Creates an ACER model with a Multi-Layer Perceptron (MLP) policy 
+#When Verbose=1, it prints training info of each episode
+model.learn(total_timesteps=10000) # Trains the model for a specified number of timesteps
 
-episodes = 10
-for episode in range(1, episodes+1):
-    state = env.reset()
-    done = False
-    score = 0 
-    
-    while not done:
-        env.render()
-        action = env.action_space.sample()
-        state, reward, done, _, info = env.step(action)
-        score += reward
+#TESTING MODEL
+evaluate_policy(model, env, n_eval_episodes=10, render=True) #Evaluates 10 episodes and renders the trained model
 
-    print('Episode:{} Score:{}'.format(episode, score))
 
+#SAVING MODEL 
 env.close()
+model.save("DQN_model")
+del model
+model = ACER.load("ACER_model", env=env) #Reloading Model to restart
+    #We Want High Variance and High Possible Mean Reward
 
