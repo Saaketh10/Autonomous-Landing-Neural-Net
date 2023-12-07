d3ec8fd90d49188629a06834fdce78c2880829f6
Saaketh10 <saakethsuvarna07@gmail.com>
2023-12-06 19:32:20 -0600

    added image file and changed to array

---
+++ RoverLanderEnvironment.py
@@ -0,0 +1,54 @@
+import gym
+from gym import spaces
+import pygame
+import numpy as np
+import cv2
+
+
+class GridWorldEnv(gym.Env):
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4} #sets render mode and renders at 4fps
+    file_path = 'C:\Python\Autonomous-Landing-Neural-Net\ldem_4.tif' #path to moon elevation data file
+    IMAGE = cv2.imread(file_path) #Reading the Image File
+    IMAGE_ARRAY = np.array(IMAGE) #Converting the Image into a NumPy array to be used
+
+
+    def __init__(self, render_mode=None, size=5): #sets grid size to 5
+        self.size = size  # The size of the square grid
+        self.window_size = 512  # The size of the PyGame window
+
+        # Observations are dictionaries with the agent's and the target's location.
+        # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
+        self.observation_space = spaces.Dict(
+            {
+                "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+            }
+        )
+
+        # We have 4 actions, corresponding to "right", "up", "left", "down"
+        self.action_space = spaces.Discrete(4)
+
+        """
+        The following dictionary maps abstract actions from `self.action_space` to 
+        the direction we will walk in if that action is taken.
+        I.e. 0 corresponds to "right", 1 to "up" etc.
+        """
+        self._action_to_direction = {
+            0: np.array([1, 0]),
+            1: np.array([0, 1]),
+            2: np.array([-1, 0]),
+            3: np.array([0, -1]),
+        }
+
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+
+        """
+        If human-rendering is used, `self.window` will be a reference
+        to the window that we draw to. `self.clock` will be a clock that is used
+        to ensure that the environment is rendered at the correct framerate in
+        human-mode. They will remain `None` until human-mode is used for the
+        first time.
+        """
+        self.window = None
+        self.clock = None
